f03f2a0b0417e736164593a855b07bd4326a20a8
Malik <36223770+2000malik@users.noreply.github.com>
2024-05-17 11:31:59 +0100

    added chakra-ui & theme set-up

---
+++ styles/theme/colors/index.js
@@ -0,0 +1,11 @@
+export const colors = {
+  primary:"#240053",
+  lightPrimary:"#FFFFFF",
+  buttonPrimary:"#8807F7",
+  secondary:"#1B0132",
+  // lightSecondary:"#8807F7",
+  primaryGreen:"#0D9488",
+  primaryRed:"#E8356D",
+  gray:"#F3F4F6",
+  white:"#FFFDF2"
+};
