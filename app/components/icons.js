ceb53f17c527fc053ea7c9549d2cb0aee498a774
Malik <36223770+2000malik@users.noreply.github.com>
2024-06-05 13:45:19 +0100

    added hanmbuger

--- app/components/icons.js
+++ app/components/icons.js
@@ -271,3 +271,27 @@ export const Instagram = ({ props }) => {
     </Icon>
   );
 };
+
+export const Hamburger = ({ props }) => {
+  return (
+    <Icon
+      width="50px"
+      height="50px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g id="Menu / Hamburger_MD">
+        <path
+          id="Vector"
+          d="M5 17H19M5 12H19M5 7H19"
+          stroke="#fff"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </Icon>
+  );
+};
